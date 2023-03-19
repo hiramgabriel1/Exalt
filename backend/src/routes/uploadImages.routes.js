653e845d5f806be7cf657d84f98fc8ec8a7921ce
itsrusty <rusty3031@gmail.com>
2023-03-18 20:29:41 -0700

    add endpoints and conex with db

--- backend/src/routes/uploadImages.routes.js
+++ backend/src/routes/uploadImages.routes.js
@@ -1,12 +1,20 @@
 import { Router } from "express";
+import { Pool } from "../../config/conex.db.js";
 
 const router = Router();
 
 // submit a image
-router.post("/upload",(req, res)=>{
-    const data = req.body;
-    console.log(data)
-    res.send(data)
+router.post("/upload", async (req, res)=>{
+    const {name, ruta, description} = req.body;
+    const [rows] = await Pool.query("INSERT INTO images (name, ruta, description) VALUES (?, ?, ?)", [name,ruta,description]);
+
+    // response
+    res.json({
+        id: rows.insertId,
+        name,
+        ruta,
+        description
+    })
 })
 
 export default router
