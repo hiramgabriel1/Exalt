86519b22b293edd93975aa4bcee13c1f6fdd2273
itsrusty <rusty3031@gmail.com>
2023-03-22 17:33:53 -0700

    22/03/2023

--- backend/src/routes/uploadImages.routes.js
+++ backend/src/routes/uploadImages.routes.js
@@ -4,17 +4,20 @@ import { Pool } from "../../config/conex.db.js";
 const router = Router();
 
 // submit a image
-router.post("/upload", async (req, res)=>{
-    const {name, ruta, description} = req.body;
-    const [rows] = await Pool.query("INSERT INTO images (name, ruta, description) VALUES (?, ?, ?)", [name,ruta,description]);
+router.post("/upload", async (req, res) => {
+  const { name, ruta, description } = req.body;
+  const [rows] = await Pool.query(
+    "INSERT INTO images (name, ruta, description) VALUES (?, ?, ?)",
+    [name, ruta, description]
+  );
 
-    // response
-    res.json({
-        id: rows.insertId,
-        name,
-        ruta,
-        description
-    })
-})
+  // response
+  res.json({
+    id: rows.insertId,
+    name,
+    ruta,
+    description,
+  });
+});
 
-export default router
+export default router;
