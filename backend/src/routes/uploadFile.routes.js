2bfc6d48a72bd14aa1ca18b687c279be8261ce7d
itsrustycode <82051017+itsrusty@users.noreply.github.com>
2023-04-02 21:21:30 -0700

    add endpoint to save a image in server with multer

--- backend/src/routes/uploadFile.routes.js
+++ backend/src/routes/uploadFile.routes.js
@@ -1,9 +1,24 @@
 import { Router } from "express";
 import { uploadFileInServer } from "../controllers/upload.js"
+import multer from "multer";
+import mimetypes from "mime-types"
 
 const router = Router();
 
-// test
-router.post("/", uploadFileInServer)
+//storage multer 
+export const storage = multer.diskStorage({
+    destination: "../public/",
+    filename: (req, file, cb)=>{
+        cb("",Date.now() + file.originalname + "." + mimeTypes.extension(file.mimetype))
+    }
+})
+
+// config multer
+const upload = multer({
+    storage: storage
+})
+
+// upload file
+router.post("/", upload.single('file'), uploadFileInServer)
 
 export default router
