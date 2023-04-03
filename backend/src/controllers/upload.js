eabd53f3369c0a31ea9f15b8eaa0bb5ec54a2af5
itsrustycode <82051017+itsrusty@users.noreply.github.com>
2023-04-02 21:21:53 -0700

    update upload.js

--- backend/src/controllers/upload.js
+++ backend/src/controllers/upload.js
@@ -1,5 +1,13 @@
 import { Pool } from "../config/conex.js"
+import { storage } from "../routes/uploadFile.routes.js"
+// import mimetypes from "mime-types"
+// import multer from "multer"
 
+// upload a file to server
 export const uploadFileInServer = (req, res) =>{
-      
+    try {
+        res.send("upload file".concat(JSON.stringify(storage))); 
+    } catch (error) {
+        res.send(error)
+    }
 }
