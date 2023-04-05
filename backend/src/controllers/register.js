7be62033c38c3bd80062d9ca6b96ebd8ada72afb
itsrustycode <82051017+itsrusty@users.noreply.github.com>
2023-04-04 22:20:17 -0700

    listo pelotudo de mierda

--- backend/src/controllers/register.js
+++ backend/src/controllers/register.js
@@ -1,6 +1,29 @@
+import { Pool } from "../config/conex.js"
 import bcrypt from "bcrypt";
-import { Pool } from "../config/conex.js";
 
-export const createNewUser = (req, res) =>{
-    res.send("testing")
+export const createUser = async () =>{
+    try {
+        // get data
+        const username = req.body.username;
+        const password = req.body.password;
+        const email = req.body.email;
+        const images = req.body.images;
+      
+          // hash password user
+          const hashPassword = await bcrypt.hash(password, 10);
+      
+          // save data user in database
+          const [rows] = await Pool.query(
+            "INSERT INTO users (username, password, email, images) VALUES (?,?,?,?)",
+            [username, hashPassword, email, images]
+          );
+      
+          // validate response
+          rows
+            ? res.json({ "message": "funciona" })
+            : res.json({ "message": res.status(500) });
+      
+        } catch (error) {
+          console.log(error);
+        }
 }
