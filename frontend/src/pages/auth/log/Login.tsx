13bb49cc8e6640eedf0a0f342569c3dea63acb2c
SebastianDeLaCruz <sebastiandelacruz335@gmail.com>
2023-04-05 13:33:47 -0300

    dale hiram pinche pendejo >:/

--- frontend/src/pages/auth/log/Login.tsx
+++ frontend/src/pages/auth/log/Login.tsx
@@ -1,5 +1,6 @@
 import {
 	Box,
+	Button,
 	FormControl,
 	FormHelperText,
 	FormLabel,
@@ -7,9 +8,14 @@ import {
 } from "@chakra-ui/react";
 
 const Login = () => {
+	const handlerSubmit = (event: React.SyntheticEvent) => {
+		event.preventDefault();
+		alert("esperando que el pinche hiram termine de resolver el conflicto");
+	};
+
 	return (
 		<Box
-			backgroundColor={"blackAlpha.800"}
+			backgroundColor={"blackAlpha.900"}
 			color={"white"}
 			minHeight={"100vh"}
 			display={"flex"}
@@ -18,11 +24,42 @@ const Login = () => {
 			alignItems={"center"}
 		>
 			<h1>Dale Hiram termina la API/REST HDP</h1>
-			<FormControl border={"1px solid white"} width={"600px"} padding={"2rem"}>
-				<FormLabel>Email address</FormLabel>
-				<Input type="text" size="sm" />
-				<FormHelperText>Hiram eres un pendejo</FormHelperText>
-			</FormControl>
+
+			<form onSubmit={handlerSubmit}>
+				<FormControl
+					display={"flex"}
+					flexDirection={"column"}
+					border={"1px solid white"}
+					width={"600px"}
+					padding={"2rem"}
+					gap={"9"}
+				>
+					<div>
+						<FormLabel>Email </FormLabel>
+						<Input type="text" size="sm" />
+						<FormHelperText>Hiram eres un pendejo</FormHelperText>
+					</div>
+					<div>
+						<FormLabel>Password</FormLabel>
+						<Input type="password" size="sm" />
+						<FormHelperText>Hiram eres un pendejo</FormHelperText>
+					</div>
+					<Button
+						alignSelf={"center"}
+						width={"200px"}
+						color={"white"}
+						backgroundColor={"transparent"}
+						border={"1px solid white"}
+						_hover={{
+							bg: "white",
+							color: "black",
+						}}
+						type="submit"
+					>
+						Enviar
+					</Button>
+				</FormControl>
+			</form>
 		</Box>
 	);
 };
